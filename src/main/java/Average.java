a1aac759b4d560969546463fce7d010a3b8d63c0
rpglitchy <rpglitchy@gmail.com>
2023-03-03 06:31:33 +0000

    Day 5 - Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        double Average;
+        for  (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        Average = Double.valueOf(sum) / nums.length;
+        return Average;
     }
 }
